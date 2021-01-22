390932ce878b4075019abe88e56707f326d1cabc
Prkrr20 <73672353+Prkrr20@users.noreply.github.com>
2021-01-22 08:26:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,9 @@
 
 money = 0;
 
-function update() {
-	document.getElementByID("money").innerHTML = money;
-}
 
 
 function increment() {
 	money+=1;
-	update();
+	document.getElementByID("money").innerHTML = money;
 }
